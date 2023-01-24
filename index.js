33c3809f4ca16c73ab66bcf95a0ebc11485cb749
TrentLe <raeburn.trent6@gmail.com>
2023-01-24 01:00:57 -0500

    Added code

---
+++ index.js
@@ -0,0 +1,42 @@
+// TODO: Include packages needed for this application
+const inquiry = require('inquirer');
+const fs = require('fs/promises')
+
+// An array of questions for user input
+const questions = () => {
+    return inquiry.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Project Title : '
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Github username : '
+    },
+    {
+        type: 'input',
+        name: 'repo',
+        message: 'Repository name : '
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Description of the project : '
+    },
+    {
+        type: 'input',
+        name: 'link',
+        message: 'Link to deployed app : '
+    },
+]);
+}
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
